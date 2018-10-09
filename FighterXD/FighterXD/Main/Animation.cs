bec846b05ac2f9b9727df483f7cf2e2f0c409e8e
IsseW <41273856+IsseW@users.noreply.github.com>
2018-10-09 13:46:01 +0200

    xd

---
+++ FighterXD/FighterXD/Main/Animation.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FighterXD.Main
+{
+    class Animation
+    {
+        public List<Texture2D> keyframes;
+    }
+}
