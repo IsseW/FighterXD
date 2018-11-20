3f6dd61e8d875e3d317a892e1ef51047bef26188
IsseW <41273856+IsseW@users.noreply.github.com>
2018-11-20 15:29:08 +0100

    xd

--- FighterXD/FighterXD/Game1.cs
+++ FighterXD/FighterXD/Game1.cs
@@ -67,7 +67,7 @@ namespace FighterXD
             //p.Collider.SetSize();
             //world.Initialize(p);
 
-            player1 = new Player(new PlayerInfo(Keys.A, Keys.D, Keys.W, Keys.Space, 30000, 1000, 0.2f, 20), new CircleCollider(1), Content.Load<Texture2D>("blob"), new Vector2(500, 0), new Vector2(100, 100));
+            player1 = new Player(new PlayerInfo(Keys.A, Keys.D, Keys.W, Keys.R, 30000, 1000, 0.2f, 20, Content.Load<Texture2D>("Bullet"), 2000, 0.1f), new CircleCollider(1), Content.Load<Texture2D>("blob"), new Vector2(500, 0), new Vector2(100, 100)) { color = Color.Green};
             player1.Collider.SetSize();
 
             Texture2D eye = Content.Load<Texture2D>("eye");
@@ -86,8 +86,18 @@ namespace FighterXD
                 Parent = player1,
                 text = "XD",
                 color = Color.Black
-            }; 
+            };
+
+            RotateTowardsMouse o = new RotateTowardsMouse() { Parent = player1 };
+
+            GameObject weapon = new GameObject(new Vector2(player1.spriteSize.Y / 5, player1.spriteSize.Y / -2), -XMath.pi/2, Content.Load<Texture2D>("Rocket_launcher"), new Vector2(100, 50)) { Parent = o};
+
+            Main.Object w = new Main.Object(new Vector2(50, 0)) { Parent = weapon };
+
+            player1.InitShootingPlace(w);
+
             world.Initialize(player1);
+            
 
             
             // player1 = new Player(Content.Load<Texture2D>("blob"), 380, 640, 4.5f, 4.5f, Content.Load<Texture2D>("eye"));
