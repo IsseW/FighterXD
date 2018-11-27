1c36d3c3be24ab102d7a0e4b6de390c0e51e6c0d
Isidor Nielsen <isidor.nielsen@learnet.se>
2018-11-27 15:41:02 +0100

    sdf

--- FighterXD/FighterXD/Game1.cs
+++ FighterXD/FighterXD/Game1.cs
@@ -61,8 +61,9 @@ namespace FighterXD
             world = new World(Content.Load<Texture2D>("background"),new Vector2(80000,80000),new Main.Viewport(new Vector2(0, 0), new Vector2(graphics.PreferredBackBufferWidth, graphics.PreferredBackBufferHeight)));
             //world.ViewportZoom = 1.01f;
 
-            InitTerrain(new Vector2(500, 400), new Vector2(500, 500), new Vector2(5, 5), Content.Load<Texture2D>("floor"), true);
-
+            //InitTerrain(new Vector2(500, 400), new Vector2(500, 500), new Vector2(5, 5), Content.Load<Texture2D>("floor"), true);
+            Terrain t = new Terrain(new Vector2(500, 400), new Vector2(1000, 900), 5, Content.Load<Texture2D>("floor"));
+            world.Initialize(t);
             //PhysicalObject p = new PhysicalObject(new CircleCollider(1), Content.Load<Texture2D>("blob"), new Vector2(500, 2000), new Vector2(1500, 1500));
             //p.Collider.SetSize();
             //world.Initialize(p);
--- FighterXD/FighterXD/Main/Terrain.cs
+++ FighterXD/FighterXD/Main/Terrain.cs
@@ -12,28 +12,82 @@ namespace FighterXD.Main
     {
         int width;
         int height;
+        TerrainObject[,] t;
 
         public Terrain(Vector2 start, Vector2 end, float blockSize, Texture2D texture) : base(start)
         {
-            Vector2 blocks = (start - end) / blockSize;
+            Vector2 blocks = (end - start) / blockSize;
             width = (int)Math.Ceiling(blocks.X);
             height = (int)Math.Ceiling(blocks.Y);
 
             Random random = new Random();
-            int[,] i = new int[width, height];
-            for (int y = 0; y <= height; y++)
+            t = new TerrainObject[width, height];
+            for (int y = 0; y < height; y++)
             {
-                for (int x = 0; x <= width; x++)
+                for (int x = 0; x < width; x++)
                 {
                     Vector2 pos = start + new Vector2(x, y) * blockSize;
 
-                    ExplodableObject p = new ExplodableObject(new RectangleCollider(new Rectangle(1, 1, 1, 1), false), texture, pos, new Vector2(blockSize)) { color = new Color(random.Next(80, 220), random.Next(182, 210), random.Next(80, 100), 255) };
+                    TerrainObject p = new TerrainObject(new RectangleCollider(new Rectangle(1, 1, 1, 1), false), texture, pos, new Vector2(blockSize), this, x, y) { color = new Color(random.Next(80, 220), random.Next(182, 210), random.Next(80, 100), 255) };
                     p.Collider.SetSize();
                     children.Add(p);
-                    i[x, y + 1]++;
-                    if (i[x + 1, y] >= 4 && children.Count >= y * width + x + 1) children[y * width + x + 1].enabled = false;
+                    t[x, y] = p;
                 }
             }
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    if (x > 0)
+                    {
+                        t[x - 1, y].neighbors++;
+                        if (t[x - 1, y].neighbors >= 4) t[x - 1, y].enabled = false;
+                    }
+                    if (x < width - 1)
+                    {
+                        t[x + 1, y].neighbors++;
+                        if (t[x + 1, y].neighbors >= 4) t[x + 1, y].enabled = false;
+                    }
+                    if (y > 0)
+                    {
+                        t[x, y - 1].neighbors++;
+                        if (t[x, y - 1].neighbors >= 4) t[x, y - 1].enabled = false;
+                    }
+                    if (y < height - 1)
+                    {
+                        t[x, y + 1].neighbors++;
+                        if (t[x, y + 1].neighbors >= 4) t[x, y + 1].enabled = false;
+                    }
+
+                }
+            }
+        }
+
+        public void OnDestroyTile(int x, int y)
+        {
+            if (x > 0 && t[x - 1, y] != null) t[x - 1, y].enabled = true;
+            if (x < width - 1 && t[x + 1, y] != null) t[x + 1, y].enabled = true;
+            if (y > 0 && t[x, y - 1] != null) t[x, y - 1].enabled = true;
+            if (y < height - 1 && t[x, y + 1] != null) t[x, y + 1].enabled = true;
+            t[x, y] = null;
+        }
+    }
+
+    class TerrainObject : ExplodableObject
+    {
+        public int neighbors;
+        Terrain terrain;
+        int x, y;
+        public TerrainObject(Collider collider, Texture2D tex, Vector2 position, Vector2 spriteSize, Terrain terrain, int x, int y) : base(collider, tex, position, spriteSize)
+        {
+            this.terrain = terrain;
+            this.x = x;
+            this.y = y;
+        }
+
+        public override void OnDestroy()
+        {
+            terrain.OnDestroyTile(x, y);
         }
     }
 }
